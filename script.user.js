10e557949a0bedf4d1fab4a8cce8f0b48c384f42
HAKOL-MILEMALA <ayala90800@gmail.com>
2026-07-08 18:25:22 +0300

    Add user script to fix card heights on Tomchei Tora
    This user script adjusts card heights and displays full text without breaking the site's design.

---
+++ script.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Fix Card Heights Tomchei Tora
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  מיישר את גובה הכרטיסים ומציג טקסט מלא מבלי לשבור את עיצוב האתר
+// @match        https://tomcheitora.org.il/*
+// @grant        GM_addStyle
+// @updateURL    https://github.com/המשתמש-שלך/tomchei-tora-fix/raw/main/script.user.js
+// @downloadURL  https://github.com/המשתמש-שלך/tomchei-tora-fix/raw/main/script.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle(`
+        div.bg-white.shadow-xl .line-clamp-2 {
+            -webkit-line-clamp: unset !important;
+            line-clamp: unset !important;
+        }
+        div.h-96.bg-white.shadow-xl.cursor-pointer {
+            height: 100% !important;
+            min-height: 24rem !important;
+        }
+    `);
+})();
